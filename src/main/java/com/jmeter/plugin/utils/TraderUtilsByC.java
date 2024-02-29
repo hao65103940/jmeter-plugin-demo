37e636785211f5f1adf725717942b6c0542bb9a2
fit2cloudzhao <452988409@qq.com>
2024-02-29 21:24:45 +0800

    fix：加载dll文件

--- src/main/java/com/jmeter/plugin/utils/TraderUtilsByC.java
+++ src/main/java/com/jmeter/plugin/utils/TraderUtilsByC.java
@@ -2,6 +2,7 @@ package com.jmeter.plugin.utils;
 
 import com.sun.jna.Library;
 import com.sun.jna.Native;
+import com.sun.jna.Pointer;
 
 /**
  * @author fit2cloudzhao
@@ -13,10 +14,13 @@ public class TraderUtilsByC {
     public interface TraderApi extends Library {
 
         // 加载链接
-        TraderApi TEST_SO = Native.load("/Users/fit2cloudzhao/Documents/MS/jmeter-plugin-demo/src/main/resources/lib/libEESTraderApi.so", TraderApi.class);
+        TraderApi TEST_SO = Native.load("D:\\IdeaProject\\jmeter-plugin-demo\\src\\main\\resources\\lib\\EESTraderApi.dll", TraderApi.class);
         // 交接接口链接
-        int connServer(String traderServerIp, int traderServerPort, String queryServerIp, int queryServerPort);
-
+        int ConnServer(String traderServerIp, int traderServerPort, EESTraderEventCallback pEvent , String queryServerIp, int queryServerPort);
+        // 定义事件回调接口
+        interface EESTraderEventCallback  extends com.sun.jna.Callback {
+            void callbackMethod(Pointer data); // 这里的callbackMethod和数据类型需要根据实际情况调整
+        }
     }
 
 
@@ -26,7 +30,7 @@ public class TraderUtilsByC {
         int traderServerPort=20000;
         String queryServerIp="101.230.113.155";
         int queryServerPort=20001;
-        int result = TraderApi.TEST_SO.connServer(traderServerIp, traderServerPort, queryServerIp, queryServerPort);
+        int result = TraderApi.TEST_SO.ConnServer(traderServerIp, traderServerPort, null,queryServerIp, queryServerPort);
         System.out.println(result);
     }
 
